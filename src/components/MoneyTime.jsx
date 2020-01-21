3dfbd334c061521a7f702e0824ae59aedfccfdda
YujiaY <jack.yujia.yuan@gmail.com>
2020-01-22 00:25:04 +1000

    Add a select form for user story 1.

--- src/components/MoneyTime.jsx
+++ src/components/MoneyTime.jsx
@@ -1,9 +1,28 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
+require('dotenv').config();
 
 function MoneyTime() {
 
   // Set dummy data for dev and test
   // TODO: Use MongoDB Atlas as data source.
+  const [topTenCoins, setTopTenCoins] = useState([
+    {name: 'Bitcoin', currentPrice: 12628.123 },
+    {name: 'Ethereum',  currentPrice: 244.863 },
+    {name: 'XRP', currentPrice: 0.344 },
+    {name: 'Bitcoin Cash', currentPrice: 500.125 },
+    {name: 'Bitcoin SV', currentPrice: 459.981 } ,
+    {name: 'Tether', currentPrice: 1.459 },
+    {name: 'Litecoin', currentPrice: 84.060 } ,
+    {name: 'EOS', currentPrice: 5.338 } ,
+    {name: 'Binance Coin', currentPrice: 25.534 },
+    {name: 'Stellar', currentPrice: 0.091 }
+  ]);
+  const [selectedCoinId, setSelectedCoinId] = useState(0);
+  const [unitToBuy, setUnitToBuy] = useState(0);
+  const [updateTime, setUpdateTime] = useState('N/A')
+  const [totalCostToBuy, setTotalCostToBuy] = useState(0);
   const [transactions, setTransactions] = useState({
     Bitcoin: [
       {
@@ -84,6 +103,39 @@ function MoneyTime() {
     setTransactions(newTransactions);
   };
 
+  async function fetchCryptoAPI(e) {
+    e.preventDefault();
+    console.log('Fetching Crypto API...');
+
+    // Below did not work very well since too many request in a shot time will let the app get banned
+    // const res = await axios.get('https://api.coinmarketcap.com/v1/ticker/?convert=aud');
+
+    // Use local proxy to fetch data since coinmarketcap.com bans Front End request using Node.js
+    const res = await axios.get('http://localhost:1368/api');
+    console.log(res.data.data);
+
+    let newTopTenCoins = [];
+    res.data.data.forEach(item => {
+      let name = item.name;
+      let currentPrice = item.quote.AUD.price;
+      newTopTenCoins.push({name, currentPrice});
+    });
+    setTopTenCoins(newTopTenCoins);
+    setUpdateTime(new Date(res.data.data[0].last_updated).toLocaleString());
+  }
+
+  function handleBuyBuyBuy(e) {
+    e.preventDefault();
+    if (unitToBuy > 0) {
+      let sum = 0;
+      console.log(selectedCoinId);
+      sum = topTenCoins[selectedCoinId].currentPrice * unitToBuy;
+      setTotalCostToBuy(sum);
+    } else {
+      alert('Please enter some number to BuyBuyBuy~~~');
+    }
+  }
+
   const cardStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(5, 1fr)',
@@ -100,6 +152,8 @@ function MoneyTime() {
           <h1>MoneyTime</h1>
         </div>
       </div>
+
+      {/*Transaction History*/}
       <div className="row">
         <div className="col">
           <div className="alert alert-success" role='alert'>
@@ -126,6 +180,47 @@ function MoneyTime() {
             )}
           </div>
         </div>
+      </div>
+      {/*Top ten coins*/}
+      <div>
+        <h3>Top Ten Coins: (Last update at: {updateTime})</h3><button  className='primary alert-primary'  onClick={(e) => fetchCryptoAPI(e)}>Update coins and prices</button>
+        <ol>
+          {topTenCoins.map((coin, index) =>
+            <li key={index}>
+              Name: {topTenCoins[index].name}.
+              Current Price: { topTenCoins[index].currentPrice || 'Not available. Please update.'}
+            </li>
+          )}
+        </ol>
+      </div>
+
+      {/*  FORM */}
+      <div>
+        <form onSubmit={(e) => handleBuyBuyBuy(e)}>
+          <label>
+            <p>Pick your favorite Coin:</p>
+            <select value={selectedCoinId} onChange={e => setSelectedCoinId(e.target.value)}>
+              {topTenCoins.map((coin, index) =>
+                <option key={index} value = {index}>{topTenCoins[index].name}</option>
+              )}
+            </select>
+            <label>Unit to buy: </label>
+            <input type="number" name="unit" onChange={e => setUnitToBuy(e.target.value)}/>
+            <label>TotalCostToBuy: {totalCostToBuy}</label>
+          </label>
+          <button
+            className='primary alert-success'
+            onClick={(e) => handleBuyBuyBuy(e)}
+          >
+            Calculate Total</button>
+          <div>
+            <button
+              className='alert alert-danger'
+              type="submit"
+            >
+              BuyBuyBuy</button>
+          </div>
+        </form>
       </div>
     </div>
   )
