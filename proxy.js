1e81826f36461cdaf743962f4779bb071fc5e7ac
YujiaY <jack.yujia.yuan@gmail.com>
2020-01-21 23:44:25 +1000

    Set up local backend service to handle the CORS ban on Javascript.

---
+++ proxy.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const rp = require('request-promise');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+app.get('/api', (req, res) => {
+
+  const requestOptions = {
+    method: 'GET',
+    uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+    qs: {
+      'start': '1',
+      'limit': '10',
+      'convert': 'AUD'
+    },
+    headers: {
+      'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY
+    },
+    json: true,
+    gzip: true
+  };
+
+  rp(requestOptions).then(response => {
+
+    console.log('API call responsed.');
+    return res.status(200).json({
+      success: true,
+      data: response.data
+    });
+  }).catch((err) => {
+    console.log('API call error:', err.message);
+  });
+
+});
+
+
+const port = process.env.PORT || 1368
+app.listen(port, () => {
+  console.log('Listening on port ',port)
+})
