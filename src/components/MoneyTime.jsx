e6a786728bb14e92249c35fa739d7e5acf30e007
YujiaY <jack.yujia.yuan@gmail.com>
2020-01-21 21:56:44 +1000

    Implement user story 4: be able to delete transactions.

--- src/components/MoneyTime.jsx
+++ src/components/MoneyTime.jsx
@@ -5,7 +5,7 @@ function MoneyTime() {
   // Set dummy data for dev and test
   // TODO: Use MongoDB Atlas as data source.
   const [transactions, setTransactions] = useState({
-    'Bitcoin': [
+    Bitcoin: [
       {
         id: 1,
         unit: 1,
@@ -15,18 +15,28 @@ function MoneyTime() {
         id: 2,
         unit: 3,
         purchasePrice: 12000
-      }
-    ],
-    'Ethereum': [
+      },
       {
         id: 3,
+        unit: 5,
+        purchasePrice: 11000
+      }
+    ],
+    Ethereum: [
+      {
+        id: 4,
         unit: 1,
         purchasePrice: 800
       },
       {
-        id: 4,
+        id: 5,
         unit: 2,
         purchasePrice: 600
+      },
+      {
+        id: 6,
+        unit: 3,
+        purchasePrice: 500
       }
     ]
   });
@@ -49,13 +59,30 @@ function MoneyTime() {
 
   function calTotalOfAll() {
     let sum = 0;
-    Object.keys(transactions).forEach(type => {
-      transactions[type].forEach(tran => {
-        sum += tran.unit * tran.purchasePrice;
-      });
-    })
+    if (Object.keys(transactions).length > 0) {
+      Object.keys(transactions).forEach(type => {
+        transactions[type].forEach(tran => {
+          sum += tran.unit * tran.purchasePrice;
+        });
+      })
+    }
     return sum;
-  }
+  };
+
+  function onDelete(type, index) {
+    const newTransactions = {
+      ...transactions,
+      [type]: [...transactions[type]]
+    };
+
+    newTransactions[type].splice(index, 1);
+
+    if (newTransactions[type].length === 0) {
+      delete newTransactions[type];
+    };
+
+    setTransactions(newTransactions);
+  };
 
   const cardStyle = {
     display: 'grid',
@@ -77,20 +104,21 @@ function MoneyTime() {
         <div className="col">
           <div className="alert alert-success" role='alert'>
             <p>Total Money you have paid: A${calTotalOfAll()}. Transactions History:</p>
-            {Object.keys(transactions).map(type =>
-              <div >
+            {Object.keys(transactions).map((type) =>
+              <div key={type}>
                 <h4 className='text-center'>
                   {type} Unit Owned: { calTotalUnitPerType(type) },
                   Total Paid: { calTotalPaidPerType(type) }
                 </h4>
 
-                <div style = { cardStyle }>
-                  { transactions[type].map(tran =>
-                    <div className='card text-center'>
+                <div style = { cardStyle } >
+                  { transactions[type].map((tran, index) =>
+                    <div className='card text-center' key={index}>
                       <p>ID: { tran.id }</p>
                       <p>Unit: { tran.unit }</p>
                       <p>Purchase Price: { tran.purchasePrice }</p>
                       <p>Total Price: { tran.unit * tran.purchasePrice }</p>
+                      <button className='alert alert-danger' onClick={() => onDelete(type, index)}>Delete</button>
                     </div>
                   )}
                 </div>
